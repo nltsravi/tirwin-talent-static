2853170096aa2f5477573b32889ab936349a842f
nltsravi <ravi@neolumina.in>
2025-02-05 16:05:30 +0530

    Trainee Registration

--- src/app/pages/auth/auth.service.ts
+++ src/app/pages/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { User } from '../../model/user.model';
 
 @Injectable({
   providedIn: "root",
@@ -11,17 +12,18 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   // Login API request
-  login(email: string, password: string): Observable<any> {
-    const body = { email, password };
-    return this.http.post(`${this.apiUrl}/auth/login`, body);
-  }
-
-
   sendOtp(email: { email: string }): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/auth/login`, email);
   }
 
   validateOtpCode(email: { email: string; otpCode: string }): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/auth/validate-otp`, email);
+  }
+
+  registerTrainee(traineeData: User): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/users`, traineeData);
+  }
+  registerTrainer(traineeData: {}): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/users`, traineeData);
   }
 }
--- src/app/pages/auth/trainee-register/trainee-register.component.ts
+++ src/app/pages/auth/trainee-register/trainee-register.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from '../../../model/user.model';
+import { Router } from '@angular/router';
+
 
 @Component({
-  selector: 'app-trainee-register',
-  templateUrl: './trainee-register.component.html',
-  styleUrls: ['./trainee-register.component.css']
+  selector: "app-trainee-register",
+  templateUrl: "./trainee-register.component.html",
+  styleUrls: ["./trainee-register.component.css"],
 })
 export class TraineeRegisterComponent implements OnInit, OnDestroy {
-  firstName: string = '';
-  lastName: string = '';
-  email: string = '';
-  jobTitle: string = '';
-  phone: string = '';
-  company: string = '';
-
+  firstName: string = "";
+  lastName: string = "";
+  email: string = "";
+  jobTitle: string = "";
+  phone: string = "";
+  linkedin_id: string = "";
+  subscriptionId:string="";
+  company: string = "";
+  userType:string="";
+  message: string = "";
+  constructor(private router: Router, private authService: AuthService) {}
   // Carousel images
   images: string[] = [
-    'https://i.ytimg.com/vi/0kfPCjk4sdg/maxresdefault.jpg',
-    'https://i.fbcd.co/products/original/business-webinar-banner-template-021-8265ba94e05234c977a44e21e96c0622d09a2e318183b9f572df8016f51440e7.jpg',
+    "https://i.ytimg.com/vi/0kfPCjk4sdg/maxresdefault.jpg",
+    "https://i.fbcd.co/products/original/business-webinar-banner-template-021-8265ba94e05234c977a44e21e96c0622d09a2e318183b9f572df8016f51440e7.jpg",
   ];
   currentImage: string = this.images[0];
   private intervalId: any;
@@ -42,14 +51,44 @@ export class TraineeRegisterComponent implements OnInit, OnDestroy {
   }
 
   registerTrainee() {
-    console.log('Registering trainee:', {
+    console.log("Registering trainee:", {
       firstName: this.firstName,
       lastName: this.lastName,
       email: this.email,
       jobTitle: this.jobTitle,
       phone: this.phone,
-      company: this.company
+      company: this.company,
     });
-    // Call API to register trainee
+
+    if (this.email) {
+      const userData: User = {
+        first_name: this.firstName,
+        last_name: this.lastName,
+        email: this.email,
+        job_title: this.jobTitle,
+        phone: this.phone,
+        company: this.company,
+        linkedin_id: this.linkedin_id,
+        user_type: 'trainee',
+        subscriptionId: "ea0f4f30-6959-433a-a4c9-6e5da7978278", //Need to remove hardcoded value
+      };
+      this.authService.registerTrainee(userData).subscribe({
+        next: (response) => {
+          console.log("User created:", response);
+          this.message = "User created successfully!";
+          alert("User Signed up successfully.Please login");
+          this.router.navigate(["/auth/login"]);
+        },
+        error: (error) => {
+          console.error("Error:", error);
+          this.message = "Error creating user";
+          alert(
+            "Error while submitting user details.Kindly verify the details"
+          );
+        },
+      });
+    } else {
+      alert("Please fill the form correctly");
+    }
   }
 }
