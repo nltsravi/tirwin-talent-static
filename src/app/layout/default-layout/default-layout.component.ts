4533c2ba3d3c751a0bb9f314f94571f1189c16b9
nltsravi <ravi@neolumina.in>
2025-09-15 21:04:38 +0530

    changes

--- src/app/layout/default-layout/default-layout.component.ts
+++ src/app/layout/default-layout/default-layout.component.ts
@@ -14,13 +14,21 @@ export class DefaultLayoutComponent implements OnInit {
   constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit() {
+    // Check initial route data on component load
+    this.updateSliderVisibility();
+    
+    // Listen for navigation events to update slider visibility
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
     ).subscribe(() => {
-      const currentRoute = this.route.snapshot.firstChild;
-      // Only show slider if explicitly set to true (home page only)
-      this.showSlider = currentRoute?.data['showSlider'] === true;
-      this.showAccount = currentRoute?.data['account'] !== false;
+      this.updateSliderVisibility();
     });
+  }
+
+  private updateSliderVisibility() {
+    const currentRoute = this.route.snapshot.firstChild;
+    // Only show slider if explicitly set to true (home page only)
+    this.showSlider = currentRoute?.data['showSlider'] === true;
+    this.showAccount = currentRoute?.data['account'] !== false;
   }
 }
